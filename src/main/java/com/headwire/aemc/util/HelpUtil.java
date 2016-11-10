a9bed490d50315ba34ed15a6b434c93750bdf22e
msaitov <msaitov@10.180.226.192>
2016-11-10 17:04:23 +0100

    template specific help, show list of all available templates for AEM templates, componenets, osgi's, models, services and servlets

--- src/main/java/com/headwire/aemc/util/HelpUtil.java
+++ src/main/java/com/headwire/aemc/util/HelpUtil.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.FalseFileFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,8 +30,13 @@ public class HelpUtil {
 
   private static final Logger LOG = LoggerFactory.getLogger(HelpUtil.class);
   public static final String AEMC_HELP_FOLDER = "help";
+  public static final String AEMC_TEMPLATE_FOLDER = "template";
+  public static final String AEMC_COMPONENT_FOLDER = "component";
+  public static final String AEMC_OSGI_FOLDER = "osgi";
+  public static final String AEMC_MODEL_FOLDER = "model";
+  public static final String AEMC_SERVICE_FOLDER = "service";
+  public static final String AEMC_SERVLET_FOLDER = "servlet";
   public static final String AEMC_HELP_FILE_START = "help-start.txt";
-  public static final String AEMC_HELP_FILE_END = "help-end.txt";
   public static final String AEMC_HELP_FILE_OPTIONS = "help-options.txt";
   public static final String AEMC_HELP_FILE_CONFIG = "help-config.txt";
   public static final String AEMC_HELP_FILE_TYPE = "help-type.txt";
@@ -73,31 +82,35 @@ public class HelpUtil {
         helpText.append(ConfigUtil.getConfigPropertiesAsText());
 
       } else if (args.length == 2) {
-        // help + arg
+        // help + <type>
         if (Constants.TYPE_APPS_UI_LIST.contains(args[1]) || Constants.TYPE_CORE_LIST.contains(args[1])) {
           helpText.append(getTextFromFile(AEMC_HELP_FILE_START));
           helpText.append(getTextFromFile(AEMC_HELP_FILE_NAME));
+          helpText.append(getTextFromFile(getTypeHelpFolder(args[1]) + "/" + AEMC_HELP_FILE_NAME));
           helpText.append(getTextFromFile(AEMC_HELP_FILE_TARGET_NAME));
+          helpText.append(getTextFromFile(getTypeHelpFolder(args[1]) + "/" + AEMC_HELP_FILE_TARGET_NAME));
           helpText.append(getTextFromFile(AEMC_HELP_FILE_ARGS));
-          // helpText.append(getTextFromFile(AEMC_HELP_FILE_END));
+          helpText.append(getTextFromFile(getTypeHelpFolder(args[1]) + "/" + AEMC_HELP_FILE_ARGS));
+          // get all available templates
+          helpText.append(getTemplates(args[1]));
         } else {
           addCompleteHelp = true;
         }
 
       } else if (args.length == 3) {
-        // help + arg + arg
         // help + <type> + <name>
         if (Constants.TYPE_APPS_UI_LIST.contains(args[1]) || Constants.TYPE_CORE_LIST.contains(args[1])) {
           helpText.append(getTextFromFile(AEMC_HELP_FILE_START));
           helpText.append(getTextFromFile(AEMC_HELP_FILE_TARGET_NAME));
+          helpText.append(getTextFromFile(getTypeHelpFolder(args[1]) + "/" + AEMC_HELP_FILE_TARGET_NAME));
           helpText.append(getTextFromFile(AEMC_HELP_FILE_ARGS));
-          // helpText.append(getTextFromFile(AEMC_HELP_FILE_END));
+          helpText.append(getTextFromFile(getTypeHelpFolder(args[1]) + "/" + AEMC_HELP_FILE_ARGS));
           // get all placeholders
           helpText.append(getPlaceHolders(args[1], args[2]));
-          // helpText.append(getTextFromFile(AEMC_HELP_FILE_END));
         } else {
           addCompleteHelp = true;
         }
+
       } else {
         // in all other cases
         addCompleteHelp = true;
@@ -114,10 +127,33 @@ public class HelpUtil {
       helpText.append(getTextFromFile(AEMC_HELP_FILE_OPTIONS));
       helpText.append(getTextFromFile(AEMC_HELP_FILE_CONFIG));
       helpText.append(getTextFromFile(AEMC_HELP_FILE_TYPE));
+
+      // name option
       helpText.append(getTextFromFile(AEMC_HELP_FILE_NAME));
+      helpText.append(getTextFromFile(AEMC_TEMPLATE_FOLDER + "/" + AEMC_HELP_FILE_NAME));
+      helpText.append(getTextFromFile(AEMC_COMPONENT_FOLDER + "/" + AEMC_HELP_FILE_NAME));
+      helpText.append(getTextFromFile(AEMC_OSGI_FOLDER + "/" + AEMC_HELP_FILE_NAME));
+      helpText.append(getTextFromFile(AEMC_MODEL_FOLDER + "/" + AEMC_HELP_FILE_NAME));
+      helpText.append(getTextFromFile(AEMC_SERVICE_FOLDER + "/" + AEMC_HELP_FILE_NAME));
+      helpText.append(getTextFromFile(AEMC_SERVLET_FOLDER + "/" + AEMC_HELP_FILE_NAME));
+
+      // targetname option
       helpText.append(getTextFromFile(AEMC_HELP_FILE_TARGET_NAME));
+      helpText.append(getTextFromFile(AEMC_TEMPLATE_FOLDER + "/" + AEMC_HELP_FILE_TARGET_NAME));
+      helpText.append(getTextFromFile(AEMC_COMPONENT_FOLDER + "/" + AEMC_HELP_FILE_TARGET_NAME));
+      helpText.append(getTextFromFile(AEMC_OSGI_FOLDER + "/" + AEMC_HELP_FILE_TARGET_NAME));
+      helpText.append(getTextFromFile(AEMC_MODEL_FOLDER + "/" + AEMC_HELP_FILE_TARGET_NAME));
+      helpText.append(getTextFromFile(AEMC_SERVICE_FOLDER + "/" + AEMC_HELP_FILE_TARGET_NAME));
+      helpText.append(getTextFromFile(AEMC_SERVLET_FOLDER + "/" + AEMC_HELP_FILE_TARGET_NAME));
+
+      // args option
       helpText.append(getTextFromFile(AEMC_HELP_FILE_ARGS));
-      // helpText.append(getTextFromFile(AEMC_HELP_FILE_END));
+      helpText.append(getTextFromFile(AEMC_TEMPLATE_FOLDER + "/" + AEMC_HELP_FILE_ARGS));
+      helpText.append(getTextFromFile(AEMC_COMPONENT_FOLDER + "/" + AEMC_HELP_FILE_ARGS));
+      helpText.append(getTextFromFile(AEMC_OSGI_FOLDER + "/" + AEMC_HELP_FILE_ARGS));
+      helpText.append(getTextFromFile(AEMC_MODEL_FOLDER + "/" + AEMC_HELP_FILE_ARGS));
+      helpText.append(getTextFromFile(AEMC_SERVICE_FOLDER + "/" + AEMC_HELP_FILE_ARGS));
+      helpText.append(getTextFromFile(AEMC_SERVLET_FOLDER + "/" + AEMC_HELP_FILE_ARGS));
     }
 
     return helpText.toString();
@@ -228,5 +264,145 @@ public class HelpUtil {
       throw new IOException(e);
     }
     return placeHolders.toString();
+  }
+
+  /**
+   * Get list of all existing templates for type.
+   *
+   * @param type
+   *          - template type
+   * @return list of all existing templates
+   * @throws IOException
+   *           - IOException
+   */
+  private static String getTemplates(final String type) throws IOException {
+    final StringBuilder templs = new StringBuilder();
+
+    // Get Config Properties from config file
+    final Properties configProps = ConfigUtil.getConfigProperties();
+    final String searchPath = ConfigUtil.getTypeSourceFolder(configProps, type);
+    final File dir = new File(searchPath);
+
+    if (!dir.exists()) {
+      templs.append("Can't get available templates. Directory " + searchPath + " doesn't exist.");
+    } else {
+      if (dir.isDirectory()) {
+        templs.append("Found next template names: \n");
+
+        // find available templates
+        final Collection<File> fileList = findTemplates(dir, type);
+        final Iterator<File> iter = fileList.iterator();
+        while (iter.hasNext()) {
+          final File nextFile = iter.next();
+          templs.append("    ");
+          templs.append(getTemplateName(dir, nextFile));
+          templs.append("\n");
+          LOG.debug("Found: {}", nextFile);
+        }
+      } else {
+        templs.append("Can't get available templates. The " + searchPath + " isn't directory.");
+      }
+    }
+
+    return templs.toString();
+  }
+
+  /**
+   * Get template name incl. subfolders.
+   *
+   * @param dir
+   *          - templates directory
+   * @param nextFile
+   *          - template file under the directory
+   * @return template name
+   */
+  private static String getTemplateName(final File dir, final File templateFile) {
+    // get template name incl. subfolders, for ex. "impl/SampleServiceImpl.java"
+    String name = StringUtils.substringAfter(templateFile.getPath(), dir.getPath());
+
+    // convert to unix path format
+    name = name.replace("\\", "/");
+
+    // cut first slash
+    if (name.indexOf("/") == 0) {
+      name = name.substring(1);
+    }
+    return name;
+  }
+
+  /**
+   * Get list of all existing templates for type.
+   *
+   * @param type
+   *          - template type
+   * @return list of all existing templates
+   * @throws IOException
+   *           - IOException
+   */
+  private static Collection<File> findTemplates(final File dir, final String type) {
+    Collection<File> fileList = new ArrayList<File>();
+
+    switch (type) {
+      case Constants.TYPE_TEMPLATE:
+      case Constants.TYPE_TEMPLATE_FULL:
+      case Constants.TYPE_COMPONENT:
+      case Constants.TYPE_COMPONENT_FULL:
+        for (final File file : FileUtils.listFilesAndDirs(dir, FalseFileFilter.INSTANCE,
+            DirectoryFileFilter.INSTANCE)) {
+          // get only root directories
+          final String name = getTemplateName(dir, file);
+          if (StringUtils.isNotBlank(name) && !name.contains("/")) {
+            fileList.add(file);
+          }
+        }
+        break;
+      case Constants.TYPE_OSGI:
+        fileList = FileUtils.listFiles(dir, new String[] { Constants.FILE_EXT_XML }, false);
+        break;
+      case Constants.TYPE_MODEL:
+      case Constants.TYPE_SERVICE:
+      case Constants.TYPE_SERVLET:
+        fileList = FileUtils.listFiles(dir, new String[] { Constants.FILE_EXT_JAVA }, true);
+        break;
+      default:
+        throw new IllegalStateException("Unknown <type> argument: " + type);
+    }
+    return fileList;
+  }
+
+  /**
+   * Get type help text folder.
+   *
+   * @param type
+   *          - template type
+   * @return type help folder
+   */
+  public static String getTypeHelpFolder(final String type) {
+    String typeHelpFolder = "";
+    switch (type) {
+      case Constants.TYPE_TEMPLATE:
+      case Constants.TYPE_TEMPLATE_FULL:
+        typeHelpFolder = AEMC_TEMPLATE_FOLDER;
+        break;
+      case Constants.TYPE_COMPONENT:
+      case Constants.TYPE_COMPONENT_FULL:
+        typeHelpFolder = AEMC_COMPONENT_FOLDER;
+        break;
+      case Constants.TYPE_OSGI:
+        typeHelpFolder = AEMC_OSGI_FOLDER;
+        break;
+      case Constants.TYPE_MODEL:
+        typeHelpFolder = AEMC_MODEL_FOLDER;
+        break;
+      case Constants.TYPE_SERVICE:
+        typeHelpFolder = AEMC_SERVICE_FOLDER;
+        break;
+      case Constants.TYPE_SERVLET:
+        typeHelpFolder = AEMC_SERVLET_FOLDER;
+        break;
+      default:
+        throw new IllegalStateException("Unknown <type> argument: " + type);
+    }
+    return typeHelpFolder;
   }
 }
