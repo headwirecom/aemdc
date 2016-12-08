bf388e78782d3900726735e08390fd6c1d5a26a6
Marat Saitov <github@ceridan.de>
2016-12-08 14:38:29 +0100

    Bugfixing to avoid exception during the path placeholder replacing

--- src/main/java/com/headwire/aemdc/command/ReplacePathPlaceHoldersCommand.java
+++ src/main/java/com/headwire/aemdc/command/ReplacePathPlaceHoldersCommand.java
@@ -91,8 +91,8 @@ public class ReplacePathPlaceHoldersCommand implements Command {
         if (!srcFile.exists()) {
           LOG.error("Can't get available templates. File {} doesn't exist.", srcFile);
         } else {
-          FileUtils.deleteQuietly(destFile);
-          FileUtils.moveFile(srcFile, destFile);
+          FileUtils.copyFile(srcFile, destFile);
+          FileUtils.deleteQuietly(srcFile);
           LOG.debug("File {} removed to {}", srcFile, destFile);
         }
       }
